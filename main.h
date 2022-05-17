0e62e68045b458c860bf87bb383b25ffea4b8341
abenet <solabenet@gmail.com>
2022-05-17 04:52:14 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+int _printf(const char *format, ...);
+int _putchar(const char c);
+int _strlen(const char *s);
+void _puts(const char *str);
+int _numlen(int n);
+#endif
