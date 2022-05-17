6284bf45207a93d69988abf005c8f7540f854571
abenet <solabenet@gmail.com>
2022-05-17 07:34:54 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,12 +1,10 @@
 #include <stdarg.h>
 #include "main.h"
 /**
-<<<<<<< HEAD
- *_prinf - print formated strings
- *@format: format of the string
- *Return: number of character printed
+ * _printf - print formated string
+ * @format: string format
+ * Return: printed characters
  */
-
 int _printf(const char *format, ...)
 {
 	va_list list;
@@ -36,58 +34,10 @@ int _printf(const char *format, ...)
 /**
  * format_values - format string
  * @list: list of args
-=======
-<<<<<<< HEAD
- * format_val - format string
- * @list: list of args
- * @printed: number of printed chars
- * @format: string format
- * @count: iterator count
- * return: operator that coresponds to a function pointer
-=======
- *_prinf - print formated strings
- *@format: format of the string
- *Return: number of character printed
- */
-
-int _printf(const char *format, ...)
-{
-	va_list list;
-	int count = 0, printed = 0;
-
-	if (!format)
-		return (-1);
-	va_start(list, format);
-	while (format && format[count])
-	{
-		if (format[count] == '%')
-		{
-			if (format[count + 1] == '\0')
-				return (-1);
-			format_val(list, format, &printed, &count);
-		}
-		else
-		{
-			_putchar(format[count]);
-			printed += 1;
-			count += 1;
-		}
-	}
-	va_end(list);
-	return (printed);
-}
-/**
- * format_val - format string
- * @list: list of args
->>>>>>> ab
  * @format: format string
  * @printed: number of chars printed
  * @count: count iterator
  * Return: pointer to func that correspond to operator
-<<<<<<< HEAD
-=======
->>>>>>> 14c87300d3db7a681bfdff19b7293a7f060367ef
->>>>>>> ab
  */
 void format_values(va_list list, const char *format, int *printed, int *count)
 {
@@ -133,85 +83,10 @@ void format_values(va_list list, const char *format, int *printed, int *count)
 		*count += 2;
 }
 /**
-<<<<<<< HEAD
- * format_string - test string format
- * @list: list of argument
- * @printed: pointer to printed chars
- * Return: void
-=======
-<<<<<<< HEAD
- * -printf - print any formated string
- * @format: string format
- * return: the number of the character printed
->>>>>>> ab
- */
-void format_string(va_list list, int *printed, char sr)
-{
-        char *s;
-        s = va_arg(list, char *);
-
-        if (s)
-        {
-                *printed += _strlen(s);
-                if (sr == 's')
-                        _puts(s);
-                else
-                        _printstr(s);
-        }
-        else
-        {
-                *printed += _strlen("(null)");
-                if (sr == 's')
-                        _puts("(null)");
-                else
-                        _printstr("(null)");
-        }
-
-/**
-<<<<<<< HEAD
-=======
  * format_int - test number formats
- * @list: args list
+ * @list: list of args
  * @printed: pointer to amount of printed chars
- * return void
-=======
- * format_string - test string format
- * @list: list of argument
- * @printed: pointer to printed chars
  * Return: void
- */
-void format_string(va_list list, int *printed, char sr)
-{
-        char *s;
-        s = va_arg(list, char *);
-
-        if (s)
-        {
-                *printed += _strlen(s);
-                if (sr == 's')
-                        _puts(s);
-                else
-                        _printstr(s);
-        }
-        else
-        {
-                *printed += _strlen("(null)");
-                if (sr == 's')
-                        _puts("(null)");
-                else
-                        _printstr("(null)");
-        }
-
-/**
->>>>>>> ab
- * format_int - tests formats
- * @list: list of argument
- * @printed: pointer to printed chars
- * Return: void
-<<<<<<< HEAD
-=======
->>>>>>> 14c87300d3db7a681bfdff19b7293a7f060367ef
->>>>>>> ab
  */
 void format_int(va_list list, int *printed)
 {
@@ -222,13 +97,11 @@ void format_int(va_list list, int *printed)
 	_printd(num);
 	*printed += _numlen(num);
 }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 /**
  * format_string - test string format
  * @list: list of args
  * @printed: pointer to amount of printed chars
+ * @sr: character string
  * Return: void
  */
 void format_string(va_list list, int *printed, char sr)
@@ -244,6 +117,7 @@ void format_string(va_list list, int *printed, char sr)
 		else
 			_printstr(s);
 	}
+
 	else
 	{
 		*printed += _strlen("(null)");
@@ -252,7 +126,4 @@ void format_string(va_list list, int *printed, char sr)
 		else
 			_printstr("(null)");
 	}
-=======
->>>>>>> 14c87300d3db7a681bfdff19b7293a7f060367ef
->>>>>>> ab
 }
